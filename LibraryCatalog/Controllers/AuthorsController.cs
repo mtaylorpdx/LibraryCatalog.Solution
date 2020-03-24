980a7d837801279fac245101b54bbf7b5fd55671
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-24 09:53:28 -0700

    add views for authors class
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>

--- LibraryCatalog/Controllers/AuthorsController.cs
+++ LibraryCatalog/Controllers/AuthorsController.cs
@@ -33,5 +33,43 @@ namespace LibraryCatalog.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      var thisAuthor = _db.Authors
+          .Include(author => author.Titles)
+          .ThenInclude(join => join.Title)
+          .FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Author author)
+    {
+      _db.Entry(author).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      return View(thisAuthor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      _db.Authors.Remove(thisAuthor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
